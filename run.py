65f30ee46e9975d22ecf9734bc1eb34d1adfc892
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-09-11 12:11:21 +0200

    ISZLC - Alpha Version - 11.09.2022

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from iszlc import app
 
 #Checks if the run.py file has executed directly and not imported
+#Sprawdza, czy plik run.py został uruchomiony bezpośrednio i nie został zaimportowany
+
 if __name__ == '__main__':
     app.run(debug=True)
