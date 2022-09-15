48f368652553f164602c038f3b8ba32b6ed21511
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-09-15 19:02:19 +0200

    15.09.2022 - Update

---
+++ iszlc/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from iszlc.models import Doctors
+
+
+class RegisterForm(FlaskForm):
+    def validate_doctor_surname(self, doctor_surname_to_check):
+        doctor_surname = Doctors.query.filter_by(doctor_surname=doctor_surname_to_check.data).first()
+        if doctor_surname:
+            raise ValidationError('Doktor aktualnie istnieje! Prosze sprobuj inne nazwisko doktora')
+
+    def validate_doctor_nr(self, doctor_nr_to_check):
+        doctor_nr = Doctors.query.filter_by(doctor_nr=doctor_nr_to_check.data).first()
+        if doctor_nr:
+            raise ValidationError('Podany aktualnie istnieje! Prosze sprobuj inny numer')
+
+
+    doctor_surname = StringField(label='Nazwisko doktora:', validators=[Length(min=2, max=30), DataRequired()])
+    doctor_name = StringField(label='Imię doktora:', validators=[Length(min=4, max=60), DataRequired()])
+    doctor_nr = StringField(label='Numer:', validators=[Length(min=4), DataRequired()])
+    
+    submit = SubmitField(label='Dodaj!')
--- iszlc/routes.py
+++ iszlc/routes.py
@@ -1,6 +1,7 @@
 from iszlc import app
 from flask import render_template, redirect, url_for, flash
 from iszlc.models import Doctors
+from iszlc.forms import RegisterForm
 from iszlc import db
 
 @app.route('/')
@@ -12,3 +13,21 @@ def home_page():
 def iszlc_page():
     doktor = Doctors.query.all()
     return render_template('iszlc.html', Doctors=doktor)
+
+@app.route('/dodaj', methods=['GET', 'POST'])
+def add_page():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        doktor_to_create = Doctors(doctor_name=form.doctor_name.data,
+                                   doctor_surname=form.doctor_surname.data,
+                                   doctor_nr=form.doctor_nr.data)
+        db.session.add(doktor_to_create)
+        db.session.commit()
+        flash(f"Doktor dodany pomyslnie!", category='success')
+        return redirect(url_for('iszlc_page'))
+
+    if form.errors != {}: #Jesli nie ma bledow z validatora
+        for err_msg in form.errors.values():
+            flash(f'Blad tworzenia uzytkownika: {err_msg}', category='danger')
+
+    return render_template('add.html', form=form)
