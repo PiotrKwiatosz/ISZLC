3921ddc95e970e927e8d273e55de68fbc91fa894
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-09-25 19:43:20 +0200

    25.09.2022 - UPDATE

--- iszlc/models.py
+++ iszlc/models.py
@@ -2,9 +2,9 @@ from iszlc import db
 
 class Doctors(db.Model):
     doctor_id = db.Column(db.Integer(), primary_key=True)
-    doctor_name = db.Column(db.String(), nullable=False, unique=False)
-    doctor_surname = db.Column(db.String(), nullable=False, unique=True)
+    doctor_name = db.Column(db.String(200), nullable=False, unique=False)
+    doctor_surname = db.Column(db.String(400), nullable=False, unique=True)
     doctor_nr = db.Column(db.Integer(), nullable=True, unique=True)
 
     def __repr__(self):
-        return f'Doktor {self.doctor_surname}'
+        return f'Doktor {self.doctor_id}'
