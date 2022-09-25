1f67ed3f54b959da53668fa30d6ce0e019684de1
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-09-25 19:43:33 +0200

    25.09.2022 - UPDATE

--- iszlc/routes.py
+++ iszlc/routes.py
@@ -1,5 +1,6 @@
+from urllib import request
 from iszlc import app
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from iszlc.models import Doctors
 from iszlc.forms import RegisterForm
 from iszlc import db
@@ -14,20 +15,21 @@ def iszlc_page():
     doktor = Doctors.query.all()
     return render_template('iszlc.html', Doctors=doktor)
 
-@app.route('/dodaj', methods=['GET', 'POST'])
+@app.route('/dodaj', methods=['POST', 'GET'])
 def add_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        doktor_to_create = Doctors(doctor_name=form.doctor_name.data,
-                                   doctor_surname=form.doctor_surname.data,
+#    if request.method == "POST":
+        doctor_to_create = Doctors(doctor_surname=form.doctor_surname.data,
+                                   doctor_name=form.doctor_name.data,
                                    doctor_nr=form.doctor_nr.data)
-        db.session.add(doktor_to_create)
+        db.session.add(doctor_to_create)
         db.session.commit()
         flash(f"Doktor dodany pomyslnie!", category='success')
         return redirect(url_for('iszlc_page'))
 
     if form.errors != {}: #Jesli nie ma bledow z validatora
         for err_msg in form.errors.values():
-            flash(f'Blad tworzenia uzytkownika: {err_msg}', category='danger')
+            flash(f'Blad dodania doktora: {err_msg}', category='danger')
 
     return render_template('add.html', form=form)
