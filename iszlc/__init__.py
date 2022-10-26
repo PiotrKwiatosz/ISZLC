9cb29dc9bcdaaed955dc499c038aeb06958f2734
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-10-26 20:16:42 +0200

    26.10.2022 - UPDATE

--- iszlc/__init__.py
+++ iszlc/__init__.py
@@ -3,8 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iszlc.db'
-app.config['SECRET_KEY'] = 'gsdget32536ywshsdyw3d'
+app.config['SECRET_KEY'] = '51883dce7428e2abbf7843b6'
 db = SQLAlchemy(app)
-
 
 from iszlc import routes
