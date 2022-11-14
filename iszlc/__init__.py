232002de7a15728aae4800a2d2da2641676691b1
Piotr Kwiatosz <piotrekkwiatosz@gmail.com>
2022-11-14 13:29:04 +0100

    14.11.2022

--- iszlc/__init__.py
+++ iszlc/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-app = Flask(__name__, instance_path='/db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iszlcsqlite3.db'
+app = Flask(__name__, instance_path = '/db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iszlc.db'
 app.config['SECRET_KEY'] = '51883dce7428e2abbf7843b6'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
